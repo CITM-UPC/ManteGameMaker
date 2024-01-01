18c153bf003a6259e9daf98d3c911da8d8a77423
Luis González <lluis.gora@gmail.com>
2024-01-01 16:36:44 +0100

    EMPTY NEW BRANCH

--- MantelGameEditor/WindowSettings.cpp
+++
@@ -1,7 +0,0 @@
-#include "WindowSettings.h"
-
-WindowSettings::WindowSettings(string name, bool startEnabled, ImGuiWindowFlags flags) : GuiBase(name, startEnabled, flags) {};
-
-void WindowSettings::Update() {
-
-}
